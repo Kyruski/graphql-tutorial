c21af44120a25f609b02daf90b52dacd474a3f87
kyruski <jamescboyett@gmail.com>
2019-03-04 16:55:48 -0600

    Adds the Link datatype and creates CRUD functionality (adding U and D) in mutations

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 const { GraphQLServer }: any = require("graphql-yoga");
 
-let links: Array<object> = [
+type Link = {
+  id: String;
+  description: String;
+  url: String;
+};
+
+let links: Array<Link> = [
   {
     id: "link-0",
     url: "www.howtographql.com",
@@ -16,14 +22,34 @@ const resolvers: object = {
     feed: () => links
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
+    createLink: (parent, args): Link => {
+      const link: Link = {
         id: `link-${idCount++}`,
         url: args.url,
         description: args.description
       };
       links.push(link);
       return link;
+    },
+    updateLink: (parent, args): Link => {
+      let item: Link;
+      for (let i = 0; i < links.length; i++) {
+        if (links[i].id === args.id) {
+          if (args.url) links[i].url = args.url;
+          if (args.description) links[i].description = args.description;
+          item = links[i];
+        }
+      }
+      return item;
+    },
+    deleteLink: (parent, args): Link => {
+      for (let i = 0; i < links.length; i++) {
+        if (links[i].id === args.id) {
+          let item: Link = links[i];
+          links.splice(i, 1);
+          return item;
+        }
+      }
     }
   }
 };
