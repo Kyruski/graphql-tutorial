d7831e9829b1dea6e627775493c9a3847a5f66dc
kyruski <jamescboyett@gmail.com>
2019-03-04 15:28:40 -0600

    Implements schema and resolvers for the feed and links

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,34 @@
-const { GraphQLServer } = require("graphql-yoga");
+const { GraphQLServer }: any = require("graphql-yoga");
 
 const typeDefs: string = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
+let links: Array<object> = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
 const resolvers: object = {
   Query: {
-    info: () => `This is the API for hackernoon`
+    info: () => `This is the API for hackernoon`,
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id,
+    url: parent => parent.url,
+    description: parent => parent.description
   }
 };
 
